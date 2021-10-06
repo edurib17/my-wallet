90e3ccfcabe0d90b7de42a9d7a2f0a4f56d6a636
edurib17 <eduardoemanuelr@gmail.com>
2021-10-06 12:13:03 -0300

    filtering inputs and outputs by month and year

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container } from "../../components/Content/styles";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
 
 const Dashboard: React.FC = () => {
-  const options = [
-    {
-      value: "Edu",
-      label: "Edu",
-    },
+  const [monthSelected, setMonthSelected] = useState<string>("");
+  const [yearSelected, setYearSelected] = useState<string>("");
+  const months = [
+    { value: 1, label: "Janeiro" },
+    { value: 2, label: "Fevereiro" },
+  ];
+  const years = [
+    { value: 2021, label: 2021 },
+    { value: 2020, label: 2020 },
   ];
   return (
     <Container>
-      <ContentHeader title="Dashboard" lineColor="#F7931B" >
-        <SelectInput options={options} />
+      <ContentHeader title="Dashboard" lineColor="#F7931B">
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(e.target.value)}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(e.target.value)}
+        />
       </ContentHeader>
     </Container>
   );
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -31,6 +31,12 @@ interface IData {
 
 const List: React.FC<IRouteParams> = ({ match }) => {
   const [data, setData] = useState<IData[]>([]);
+  const [monthSelected, setMonthSelected] = useState<string>(
+    String(new Date().getMonth() + 1)
+  );
+  const [yearSelected, setYearSelected] = useState<string>(
+    String(new Date().getFullYear())
+  );
   const { type } = match.params;
 
   const title = useMemo(() => {
@@ -48,6 +54,14 @@ const List: React.FC<IRouteParams> = ({ match }) => {
   const months = [
     { value: 1, label: "Janeiro" },
     { value: 2, label: "Fevereiro" },
+    { value: 3, label: "Março" },
+    { value: 4, label: "Abril" },
+    { value: 5, label: "Maio" },
+    { value: 6, label: "Junho" },
+    { value: 7, label: "Julho" },
+    { value: 8, label: "Agosto" },
+    { value: 9, label: "Setembro" },
+    { value: 10, label: "Outubro" },
   ];
   const years = [
     { value: 2021, label: 2021 },
@@ -55,9 +69,16 @@ const List: React.FC<IRouteParams> = ({ match }) => {
   ];
 
   useEffect(() => {
-    const response = listData.map((item) => {
+    const filteredDate = listData.filter((item) => {
+      const date = new Date(item.date);
+      const month = String(date.getMonth() + 1);
+      const year = String(date.getFullYear());
+      return month === monthSelected && year === yearSelected;
+    });
+
+    const formattedData = filteredDate.map((item) => {
       return {
-        _id: String(Math.random() * data.length),
+        _id: String(new Date().getTime()) + item.amount,
         description: item.description,
         amountFormatted: formatCurrency(Number(item.amount)),
         frequency: item.frequency,
@@ -65,13 +86,21 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         tagColor: item.frequency === "recorrente" ? "#4E41F0" : "#E44C4E",
       };
     });
-    setData(response);
-  }, []);
+    setData(formattedData);
+  }, [listData, monthSelected, yearSelected, data.length]);
   return (
     <Container>
       <ContentHeader title={title} lineColor={lineColor}>
-        <SelectInput options={months} />
-        <SelectInput options={years} />
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
       <Filters>
         <button type="button" className="tag-filter tag-filter-recurrent">
