93db3907ae0c5d8075e30a73f9e1a1b48ebf80fb
edurib17 <eduardoemanuelr@gmail.com>
2021-10-07 15:01:08 -0300

    creating wallet transaction cards

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,32 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
-import { Container } from "../../components/Content/styles";
 import ContentHeader from "../../components/ContentHeader";
 import SelectInput from "../../components/SelectInput";
+import WalletBox from "../../components/WalletBox";
+
+import listOfMonths from "../../utils/months";
+
+import gains from "../repositories/gains";
+import expenses from "../repositories/expenses";
+
+import { Container, Content } from "./styles";
 
 const Dashboard: React.FC = () => {
-  const [monthSelected, setMonthSelected] = useState<string>("");
-  const [yearSelected, setYearSelected] = useState<string>("");
-  const months = [
-    { value: 1, label: "Janeiro" },
-    { value: 2, label: "Fevereiro" },
-  ];
-  const years = [
-    { value: 2021, label: 2021 },
-    { value: 2020, label: 2020 },
-  ];
+  const [monthSelected, setMonthSelected] = useState<number>(
+    new Date().getMonth() + 1
+  );
+
+  const [yearSelected, setYearSelected] = useState<number>(
+    new Date().getFullYear()
+  );
+
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    [...expenses, ...gains].forEach((item) => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map((year) => {
+      return {
+        value: year,
+        label: year,
+      };
+    });
+  }, []);
+
+  const months = useMemo(() => {
+    return listOfMonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month,
+      };
+    });
+  }, []);
+
+  const handleMonthSelected = (month: string) => {
+    try {
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch {
+      throw new Error("invalid month value. Is accept 0 - 24.");
+    }
+  };
+
+  const handleYearSelected = (year: string) => {
+    try {
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch {
+      throw new Error("invalid year value. Is accept integer numbers.");
+    }
+  };
+
   return (
     <Container>
       <ContentHeader title="Dashboard" lineColor="#F7931B">
         <SelectInput
           options={months}
-          onChange={(e) => setMonthSelected(e.target.value)}
+          onChange={(e) => handleMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
         />
         <SelectInput
           options={years}
-          onChange={(e) => setYearSelected(e.target.value)}
+          onChange={(e) => handleYearSelected(e.target.value)}
+          defaultValue={yearSelected}
         />
       </ContentHeader>
+      <Content>
+        <WalletBox
+          title="Saldo"
+          color="#4E41F0"
+          amount={150.0}
+          footerLabel="Atualizado com base nas entradas e saídas"
+          icon="dollar"
+        />
+        <WalletBox
+          title="Entradas"
+          color="#F7931B"
+          amount={5000.0}
+          footerLabel="Atualizado com base nas entradas e saídas"
+          icon="arrowUp"
+        />
+        <WalletBox
+          title="Saídas"
+          color="#E44C4E"
+          amount={150.0}
+          footerLabel="Atualizado com base nas entradas e saídas"
+          icon="arrowDown"
+        />
+      </Content>
     </Container>
   );
 };
