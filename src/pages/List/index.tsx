168eb23bac070fdde5c2a82f0b0365bd31675a0e
edurib17 <eduardoemanuelr@gmail.com>
2021-10-07 12:34:44 -0300

    filtering by type of financial movement

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -39,6 +39,10 @@ const List: React.FC<IRouteParams> = ({ match }) => {
   const [yearSelected, setYearSelected] = useState<string>(
     String(new Date().getFullYear())
   );
+  const [selectedFrequency, setSelectedFrequency] = useState([
+    "recorrente",
+    "eventual",
+  ]);
   const { type } = match.params;
 
   const title = useMemo(() => {
@@ -79,12 +83,28 @@ const List: React.FC<IRouteParams> = ({ match }) => {
     });
   }, [listData]);
 
+  const handleFrequencyClick = (frequency: string) => {
+    const alreadySelected = selectedFrequency.findIndex(
+      (item) => item === frequency
+    );
+    if (alreadySelected >= 0) {
+      const filtered = selectedFrequency.filter((item) => item !== frequency);
+      setSelectedFrequency(filtered);
+    } else {
+      setSelectedFrequency((prev) => [...prev, frequency]);
+    }
+  };
+
   useEffect(() => {
     const filteredDate = listData.filter((item) => {
       const date = new Date(item.date);
       const month = String(date.getMonth() + 1);
       const year = String(date.getFullYear());
-      return month === monthSelected && year === yearSelected;
+      return (
+        month === monthSelected &&
+        year === yearSelected &&
+        selectedFrequency.includes(item.frequency)
+      );
     });
 
     const formattedData = filteredDate.map((item) => {
@@ -98,7 +118,7 @@ const List: React.FC<IRouteParams> = ({ match }) => {
       };
     });
     setData(formattedData);
-  }, [listData, monthSelected, yearSelected, data.length]);
+  }, [listData, monthSelected, yearSelected, data.length,selectedFrequency]);
 
   return (
     <Container>
@@ -115,10 +135,22 @@ const List: React.FC<IRouteParams> = ({ match }) => {
         />
       </ContentHeader>
       <Filters>
-        <button type="button" className="tag-filter tag-filter-recurrent">
+        <button
+          type="button"
+          className={`tag-filter tag-filter-recurrent ${
+            selectedFrequency.includes("recorrente") && "tag-actived"
+          }`}
+          onClick={() => handleFrequencyClick("recorrente")}
+        >
           Recorrentes
         </button>
-        <button type="button" className="tag-filter tag-filter-eventual">
+        <button
+          type="button"
+          className={`tag-filter tag-filter-eventual ${
+            selectedFrequency.includes("eventual") && "tag-actived"
+          }`}
+          onClick={() => handleFrequencyClick("eventual")}
+        >
           Eventuais
         </button>
       </Filters>
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -17,6 +17,7 @@ export const Filters = styled.div`
     color: ${(props) => props.theme.colors.white};
     margin: 0 10px;
     transition: 0.3s;
+    opacity: 0.4;
 
     &:hover {
       opacity: 0.7;
@@ -27,7 +28,7 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+    border-bottom: 10px solid ${(props) => props.theme.colors.success};
   }
 
   .tag-filter-eventual::after {
@@ -35,6 +36,10 @@ export const Filters = styled.div`
     display: block;
     width: 55px;
     margin: 0 auto;
-    border-bottom: 10px solid ${(props) => props.theme.colors.success};
+    border-bottom: 10px solid ${(props) => props.theme.colors.warning};
+  }
+
+  .tag-actived {
+    opacity: 1;
   }
 `;
